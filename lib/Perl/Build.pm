30b5b0fe04a533d2fd27de620851bc2888500f96
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-04-05 08:37:36 +0000

    Import of TOKUHIROM/Perl-Build-0.17 from CPAN.
    gitpan-cpan-distribution: Perl-Build
    gitpan-cpan-version:      0.17
    gitpan-cpan-path:         TOKUHIROM/Perl-Build-0.17.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Perl/Build.pm
+++ lib/Perl/Build.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 use 5.008002;
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 use Carp ();
 use File::Basename;
@@ -210,9 +210,9 @@ sub install {
         }
         $class->do_system('make install');
     }
-    return Perl::Build::Built->new(
+    return Perl::Build::Built->new({
         installed_path => $dst_path,
-    );
+    });
 }
 
 sub do_system {
